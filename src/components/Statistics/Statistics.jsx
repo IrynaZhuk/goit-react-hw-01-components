ee90fb88990bc2d5e1463c6f1ffc1a8d9e8fec16
IrynaZhuk <irynazh94@gmail.com>
2022-08-04 11:55:46 +0300

    stats/final

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,31 @@
+import PropTypes from 'prop-types';
+import { getRandomHexColor } from './getRandomHexColor';
+import { StatisticsCard, Title, StatList, StatItem, Label, Percentage } from './Statistics.styled';
+
+
 export const Statistics = ({title, stats}) => {
-    const {label, percentage} = stats;
-    return (<section class="statistics">
-    <h2 class="title">{title}</h2>
-  
-    <ul class="stat-list">
-      <li class="item">
-        <span class="label">{label}</span>
-        <span class="percentage">{percentage}</span>
-      </li>
-      <li class="item">
-        <span class="label">{label}</span>
-        <span class="percentage">{percentage}</span>
-      </li>
-      <li class="item">
-        <span class="label">{label}</span>
-        <span class="percentage">{percentage}</span>
-      </li>
-      <li class="item">
-        <span class="label">{label}</span>
-        <span class="percentage">{percentage}</span>
-      </li>
-    </ul>
-  </section>)
+    
+    return (<StatisticsCard>
+  {title && <Title>{title}</Title>}
+    <StatList>
+     {stats.map(item => ( 
+     <StatItem key={item.id}
+     style={{ backgroundColor: getRandomHexColor() }}>
+        <Label>{item.label}</Label>
+        <Percentage>{item.percentage}%</Percentage>
+      </StatItem>
+      ))}
+    </StatList>
+  </StatisticsCard>)
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        })
+    )
 }
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const StatisticsCard = styled.section`
+display: block;
+width: 360px;
+margin-left: auto;
+margin-right: auto;
+margin-top: 20px;
+border-radius: 8px;
+border: 2px black solid;
+`;
+
+export const Title = styled.h2`
+text-align: center;
+`;
+
+export const StatList = styled.ul`
+display: flex;
+`;
+
+export const StatItem = styled.li`
+display: flex;
+flex-direction: column;
+padding: 20px 0;
+justify-content: space-around;
+align-items: center;
+width: 100%;
+`;
+
+export const Label = styled.span``;
+
+export const Percentage = styled.span``;
+
