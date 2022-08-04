427a4d3a12bc2429c38fc8092844d7306b376328
IrynaZhuk <irynazh94@gmail.com>
2022-08-04 10:45:35 +0300

    json-changes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,20 +5,20 @@ export const Statistics = ({title, stats}) => {
   
     <ul class="stat-list">
       <li class="item">
-        <span class="label">.docx</span>
-        <span class="percentage">4%</span>
+        <span class="label">{label}</span>
+        <span class="percentage">{percentage}</span>
       </li>
       <li class="item">
-        <span class="label">.mp3</span>
-        <span class="percentage">14%</span>
+        <span class="label">{label}</span>
+        <span class="percentage">{percentage}</span>
       </li>
       <li class="item">
-        <span class="label">.pdf</span>
-        <span class="percentage">41%</span>
+        <span class="label">{label}</span>
+        <span class="percentage">{percentage}</span>
       </li>
       <li class="item">
-        <span class="label">.mp4</span>
-        <span class="percentage">12%</span>
+        <span class="label">{label}</span>
+        <span class="percentage">{percentage}</span>
       </li>
     </ul>
   </section>)
